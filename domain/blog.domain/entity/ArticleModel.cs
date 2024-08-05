6874df532929f6be426ae7995900cd8fd988a55b
msolhnia <m.solhnia@gmail.com>
2024-08-06 01:39:52 +0330

    model and relations created in  domai layer

---
+++ domain/blog.domain/entity/ArticleModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog.domain.entity
+{
+    public class ArticleModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public List<QuizModel> Quizzes { get; set; }   
+    }
+}
---
+++ domain/blog.domain/entity/QuizModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog.domain.entity
+{
+    public class QuizModel
+    {
+        [Key]
+        public int Id { get; set; }        
+        public string Question { get; set; }
+        public string Options { get; set; }
+        public string CorrectAnswer { get; set; }
+        public ArticleModel Article { get; set; }   
+    }
+}
