bdfd25379540ab0899f184bff8caee09d32a18d1
msolhnia <m.solhnia@gmail.com>
2024-08-06 15:32:24 +0330

    sql,core package added to project and bcontext created

---
+++ src/infrastructure/blog.infrastructure/Data/Persist/FarirContext.cs
@@ -0,0 +1,25 @@
+﻿using blog.domain.entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog.infrastructure.Data.Persist
+{
+    public class FarirContext: DbContext
+    {
+        public DbSet<ArticleModel> Articles { get; set; }
+        public DbSet<QuizModel> Quizzes { get; set; }
+
+        public FarirContext(DbContextOptions<FarirContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+    }
+}
